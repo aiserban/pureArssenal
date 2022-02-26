5e6d90a4ae730034181bad0a7553636f6eda042e
Andrei Serban <aiserban@outlook.com>
2022-02-27 00:03:18 +0200

    settings button that does nothing

--- App.tsx
+++ App.tsx
@@ -1,28 +1,48 @@
 import * as React from 'react';
-
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DashboardScreen from './src/screens/dashboardScreen';
 import {ArticleScreen} from './src/screens/articleScreen';
-import {useEffect} from 'react';
+import {Button, Pressable} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {PlatformColor} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{headerShown: true}}
-          initialRouteName={'DashboardScreen'}>
-          <Stack.Screen name={'DashboardScreen'} component={DashboardScreen} />
-          <Stack.Screen
-            name={'ArticleScreen'}
-            component={ArticleScreen}
-            options={{headerShown: false, title: ''}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+export default function App({navigation}) {
+  Icon.loadFont();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: true}}
+        initialRouteName={'DashboardScreen'}>
+        <Stack.Screen
+          name={'DashboardScreen'}
+          component={DashboardScreen}
+          options={{
+            headerLeft: () => {
+              return (
+                <Pressable
+                  onPress={() => {
+                    console.log('clicked');
+                  }}>
+                  <Icon
+                    name={'cog-outline'}
+                    color={PlatformColor('systemBlue')}
+                    size={26}
+                  />
+                </Pressable>
+              );
+            },
+          }}
+        />
+        <Stack.Screen
+          name={'ArticleScreen'}
+          component={ArticleScreen}
+          options={{headerShown: false, title: ''}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
