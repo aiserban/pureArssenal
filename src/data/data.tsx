754da19098c5de4c0b57a526eebe040dd0f0e1b4
Andrei Serban <aiserban@outlook.com>
2022-06-22 19:32:52 +0300

    minor change

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -7,7 +7,7 @@ export let FeedList: Feed[] = [];
 export let FeedListUrls: string[] = [
   // 'https://hotnews.ro/rss',
   'https://digi24.ro/rss',
-  'https://sport.ro/rss',
+  'https://www.gsp.ro/rss/fotbal-99.xml',
   'https://protv.ro/rss'
 ];
 export let ReadIdList: string[] = [];
