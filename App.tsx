51a22643874689af615c351d31e75599bd9a258d
Andrei Serban <aiserban@outlook.com>
2023-02-04 20:25:13 +0200

    minor changes to font loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DashboardScreen from './src/screens/dashboardScreen';
-import { ArticleScreen } from './src/screens/articleScreen';
-import { Pressable } from 'react-native';
+import {ArticleScreen} from './src/screens/articleScreen';
+import {PlatformColor, Pressable} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { PlatformColor } from 'react-native';
 import SettingsScreen from './src/screens/settingsScreen';
 import AddScreen from './src/screens/addScreen';
 
@@ -13,52 +12,49 @@ const Stack = createNativeStackNavigator();
 const RootStack = createNativeStackNavigator();
 
 export default function App() {
-  Icon.loadFont();
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName={'DashboardScreen'}
+                screenOptions={{headerShown: true}}>
 
-  return (
-    <NavigationContainer>
-
-      <Stack.Navigator
-        initialRouteName={'DashboardScreen'}
-        screenOptions={{ headerShown: true }}>
-
-        {/* <RootStack.Group screenOptions={{ presentation: 'modal' }}> */}
-          <Stack.Screen name={'AddScreen'} component={AddScreen} />
-        {/* </RootStack.Group> */}
+                {/* <RootStack.Group screenOptions={{ presentation: 'modal' }}> */}
+                <Stack.Screen name={'AddScreen'} component={AddScreen}/>
+                {/* </RootStack.Group> */}
 
 
-        <Stack.Screen name={'SettingsScreen'}
-          component={SettingsScreen}
-          options={({ navigation }) => ({
-            headerRight: () => <Pressable onPress={() => navigation.navigate('AddScreen')}>
-              <Icon
-                name={'add'}
-                color={PlatformColor('systemBlue')}
-                size={26}
-              />
-            </Pressable>
-          })} />
+                <Stack.Screen name={'SettingsScreen'}
+                              component={SettingsScreen}
+                              options={({navigation}) => ({
+                                  headerRight: () => <Pressable onPress={() => navigation.navigate('AddScreen')}>
+                                      <Icon
+                                          name={'add'}
+                                          color={PlatformColor('systemBlue')}
+                                          size={26}
+                                      />
+                                  </Pressable>
+                              })}/>
 
-        <Stack.Screen
-          name={'DashboardScreen'}
-          component={DashboardScreen}
-          options={({ navigation }) => ({
-            headerLeft: () => <Pressable onPress={() => navigation.navigate('SettingsScreen')}>
-              <Icon
-                name={'cog-outline'}
-                color={PlatformColor('systemBlue')}
-                size={26}
-              />
-            </Pressable>
-          })}
-        />
+                <Stack.Screen
+                    name={'DashboardScreen'}
+                    component={DashboardScreen}
+                    options={({navigation}) => ({
+                        headerLeft: () => <Pressable onPress={() => navigation.navigate('SettingsScreen')}>
+                            <Icon
+                                name={'cog-outline'}
+                                color={PlatformColor('systemBlue')}
+                                size={26}
+                            />
+                        </Pressable>
+                    })}
+                />
 
-        <Stack.Screen
-          name={'ArticleScreen'}
-          component={ArticleScreen}
-          options={{ headerShown: false, title: '' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+                <Stack.Screen
+                    name={'ArticleScreen'}
+                    component={ArticleScreen}
+                    options={{headerShown: false, title: ''}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /**
  * @format
  */
- import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Icon from "react-native-vector-icons/Ionicons";
+
+Icon.loadFont().catch(err => {
+    console.log(err);
+})
 
 AppRegistry.registerComponent(appName, () => App);
